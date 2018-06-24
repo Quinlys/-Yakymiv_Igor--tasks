88e8bcd5489b8555f77c35e865310091f282695e
Igor Yakymiv <yakymiv.ig@gmail.com>
2018-06-25 00:54:38 +0300

    Task-2 solution

--- task_2/__init__.py
+++ task_2/__init__.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*- 
 
 
-def have_same_items(list1, list2):
-    """ Проверяет состоят ли массивы list1 и list2 из одинакового
-        числа одних и тех же элементов
+def have_same_items(list1,list2):
+    list1 = sorted(list1)
+    list2 = sorted(list2)
+    
+    if list1 == list2:
+        return True
+    else:
+        return False
 
-        Parameters
-        ----------
-        list1 : list[int]
-            отсортированный по возрастанию массив уникальных элементов
-        list2 : list[int]
-            массив произвольной длинны произвольных чисел
-
-        Returns
-        -------
-        bool
-    """
-    return True
