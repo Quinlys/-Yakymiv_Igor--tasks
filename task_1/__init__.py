bacf438a78c8e29e9521e891f272da7524ce59c2
Igor <32520169+quinlys@users.noreply.github.com>
2018-06-25 00:47:30 +0300

    Task-1 solution

--- task_1/__init__.py
+++ task_1/__init__.py
@@ -1,19 +1,17 @@
-# -*- coding: utf-8 -*- 
-
-
+# -*- coding: utf-8 -*-
 def fill_spiral_matrix(n):
-    """ Создает мартицу размером n * n и заполняет ее по спирали
-
-    Parameters
-    ----------
-    n : int
-        Размерность матрицы.
-
-    Returns
-    -------
-    list[list[int]]
-        Матрица заполненная по спирали
-    """
-    result = [[]]
-
+    result = []
+    x, y, dx, dy = 0, 0, 1, 0
+    arr = [[0] * n for i in range(n)]
+    for i in range(1, n**2+1):
+        arr[x][y] = i
+        nx, ny = x+dx, y+dy
+        if 0 <= nx < n and 0 <= ny < n and not arr[nx][ny]:
+            x, y = nx, ny
+        else:
+            dx, dy = -dy, dx
+            x, y = x+dx, y+dy
+    for x in list(zip(*arr)):
+        result.append(list(x))
     return result
+
