e8ba975d489c68d329a03190d223e8817fb3a699
Igor <32520169+quinlys@users.noreply.github.com>
2018-06-25 00:43:26 +0300

    Inital commit

---
+++ task_1/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*- 
+
+
+def fill_spiral_matrix(n):
+    """ Создает мартицу размером n * n и заполняет ее по спирали
+
+    Parameters
+    ----------
+    n : int
+        Размерность матрицы.
+
+    Returns
+    -------
+    list[list[int]]
+        Матрица заполненная по спирали
+    """
+    result = [[]]
+
+    return result
